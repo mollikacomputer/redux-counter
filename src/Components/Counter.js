ad12e1cac3103efb4768dc917c21cedbaeb19f1a
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-15 22:59:46 +0600

    complex state management without redux

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,36 +1,18 @@
 import React, { useState } from 'react';
+import Button from './Button';
+import Count from './Count';
 
-const Counter = () => {
-    const [count, setCount] =  useState(0);
-
-  const handleIncrement = () =>{
-    setCount(previousCount => previousCount + 1);
-  }
-  
-  const handleDecrement = () =>{
-    setCount(previousCount => previousCount - 1);
-  }
+const Counter = ({id, count, increment, decrement}) => {
+   
 
     return (
-        <div
-                    className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-                >
-                    <div className="text-2xl font-semibold">{count}</div>
-                    <div className="flex space-x-3">
-                        <button
-                            onClick={handleIncrement}
-                            className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                        >
-                            Increment
-                        </button>
-                        <button
-                            onClick={handleDecrement}
-                            className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                        >
-                            Decrement
-                        </button>
-                    </div>
-                </div>
+        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+        <Count count={count} />
+        <div class="flex space-x-3">
+            <Button handler={() => increment(id)}>Increment</Button>
+            <Button handler={() => decrement(id)}>Decrement</Button>
+        </div>
+    </div>
     );
 };
 
